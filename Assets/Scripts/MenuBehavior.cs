536a4d01623ad58961365986488ddbcd0b9dafe2
Pranav Krishnamurthy <pkrish19@outlook.com>
2017-08-30 14:58:56 -0400

    All of the latest changes
    Preparing for the Web build

---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Young Indian Culture Group Inc. 5013(c)
+ * 
+ * Copyright (c) 2017
+ * 
+ * Author       :   Pranav S. Krishnamurthy; Japesh Shah
+ * 
+ * Project      :   Karna's Maze
+ * 
+ * File Name    :   GameDescriptionBehavior.cs
+ * 
+ * Date Created :   30th August 2017
+ * 
+ * Purpose      :   This script will be able to have all of the UI functionality to determine the transition
+ *                  from the game description to the actual game called "Game"
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehavior : MonoBehaviour {
+
+	public void progressTrigger(int trig)
+    {
+        switch (trig)
+        {
+            case (0):
+                SceneManager.LoadScene("Game"); 
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Camera : MonoBehaviour {
+public class PlayerCamera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
